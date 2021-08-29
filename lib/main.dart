f84c395c4d6b6ba78f2bc1fb4f2f7d60599a164b
iampato <858wpwaweru@gmail.com>
2021-08-29 23:58:44 +0300

    DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import 'home_page.dart';
+import 'package:huduma/screens/history_page.dart';
+import 'package:huduma/utils/moor_utils.dart';
+import 'package:provider/provider.dart';
+import 'cubit/huduma_repo_dao.dart';
+import 'screens/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,17 +29,30 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-        fontFamily: GoogleFonts.openSans().fontFamily,
-        appBarTheme: AppBarTheme(
-          centerTitle: true,
-          elevation: 0.0,
+    return Provider<HudumaResponseDao>(
+      create: (_) => MyDatabase().hudumaResponseDao,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          fontFamily: GoogleFonts.openSans().fontFamily,
+          appBarTheme: AppBarTheme(
+            centerTitle: true,
+            elevation: 0.0,
+            brightness: Brightness.dark,
+          ),
         ),
+        initialRoute: "/",
+        onGenerateRoute: (settings) {
+          if (settings.name == "/") {
+            return CupertinoPageRoute(builder: (_) => MyHomePage());
+          } else if (settings.name == "/history") {
+            return CupertinoPageRoute(builder: (_) => HistoryPage());
+          } else {
+            return null;
+          }
+        },
       ),
-      home: MyHomePage(),
     );
   }
 }
